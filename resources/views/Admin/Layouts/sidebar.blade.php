a69a4f8b0e970c770a5d939a9896615ff01a3610
Ruban <rubanpathak706@gmail.com>
2024-04-10 09:21:27 +0530

    Admin Dashboard Sidebar

--- resources/views/Admin/Layouts/sidebar.blade.php
+++ resources/views/Admin/Layouts/sidebar.blade.php
@@ -13,7 +13,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="dashboard.html" class="nav-link">
+                    <a href="{{route('admin.home')}}" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>																
